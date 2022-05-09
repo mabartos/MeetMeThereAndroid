6c927576fcc5da160f09be4d2f06996292db1d2b
Martin Bartoš <mabartos@redhat.com>
2022-05-09 23:49:56 +0200

    Add initial README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Meet Me there application
+
+## WIP
+
+<img style="float: left;" src="img/intro_app.png"/>
+<img style="float: center;" src="img/event_list.png"/>
+<img src="img/event_detail.png"/>
--- app/src/main/java/org/mabartos/meetmethere/service/event/TestEventService.kt
+++ app/src/main/java/org/mabartos/meetmethere/service/event/TestEventService.kt
@@ -19,7 +19,7 @@ class TestEventService : EventService {
                     id = it.toLong(),
                     title = "Event $it",
                     description = "Description for event $it. This event is on a special venue and you need to bring your own instruments there. We have wine, beer and some kind of drugs.",
-                    venue = "Amsterdam",
+                    venue = "Brno",
                     imageUrl = if (it % 4 > 0) "https://thumbs.dreamstime.com/z/bottles-famous-global-beer-brands-poznan-pol-mar-including-heineken-becks-bud-miller-corona-stella-artois-san-miguel-143170440.jpg" else "",
                     isPublic = it % 2 == 0,
                     createdByName = "User$it Doe",
